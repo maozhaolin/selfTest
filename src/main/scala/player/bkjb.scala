b8a850e9b3c31fbc6138aeaee168c8ed793b82b3
maozhaolin <mao18698726900@163.com>
2020-11-05 13:49:35 +0800

    Idea Git测试

--- src/main/scala/player/bkjb.scala
+++ src/main/scala/player/bkjb.scala
@@ -1,8 +1,14 @@
 package player
 
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
+
 object bkjb {
   def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("lla").setMaster("local[*]")
+    val spark = SparkSession.builder().config(conf).getOrCreate()
 
-    println("xhajkcnk")
+
+    spark.close()
   }
 }
