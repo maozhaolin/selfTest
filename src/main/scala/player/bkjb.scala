d2c6e62191d08fb5ba163066feeaa55c7fef3426
maozhaolin <mao18698726900@163.com>
2020-11-05 15:09:40 +0800

    Idea Git测试

--- src/main/scala/player/bkjb.scala
+++ src/main/scala/player/bkjb.scala
@@ -1,14 +1,27 @@
 package player
 
-import org.apache.spark.SparkConf
+
+import kafka.serializer.StringDecoder
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
 
 object bkjb {
   def main(args: Array[String]): Unit = {
-    val conf = new SparkConf().setAppName("lla").setMaster("local[*]")
-    val spark = SparkSession.builder().config(conf).getOrCreate()
-
-
-    spark.close()
+    val spark = SparkSession.builder().appName("spark_kafka").master("local[*]").getOrCreate()
+    val batchDuration = Seconds(5) //时间单位为秒
+    val streamContext = new StreamingContext(spark.sparkContext, batchDuration)
+    streamContext.checkpoint("/Users/eric/SparkWorkspace/checkpoint")
+    val topics = Array("test").toSet
+    val kafkaParams = Map[String, String]("metadata.broker.list" -> "Hadoop001:9092,Hadoop002:9092,Hadoop003:9092")
+    val stream = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](streamContext, kafkaParams, topics)
+    stream.foreachRDD(rdd => {
+      rdd.foreach(line => {
+        println("key=" + line._1 + "  value=" + line._2)
+      })
+    })
+    streamContext.start()  //spark stream系统启动
+    streamContext.awaitTermination() //
   }
 }
